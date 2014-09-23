8e516320035621ba0ed11370cf8db10b3d1740e5
itegulov <wibkwibk@gmail.com>
2014-09-23 09:59:30 +0400

    added equals to Assumption

--- src/proof/Assumption.java
+++ src/proof/Assumption.java
@@ -5,6 +5,15 @@ import expression.Expression;
 public final class Assumption implements StatementType {
     //TODO: javadoc
     //TODO: realize assumption
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        return true;
+    }
+
     @Override
     public String toString() {
         return "Допущение";
